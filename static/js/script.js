07356c28af0fd99f8c5709c8bfafcef3ae0c460f
grisselfaura <grissel.faura@gmail.com>
2020-09-18 21:05:06 +0000

    Refreshed images for carrousel

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
-    $('.collapsible').collapsible();
+    $(".collapsible").collapsible();
+    $(".carousel").carousel();
     
-  });
+});
 
-$('.carousel.carousel-slider').carousel({
+$(".carousel.carousel-slider").carousel({
     fullWidth: true
-  });
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
       
