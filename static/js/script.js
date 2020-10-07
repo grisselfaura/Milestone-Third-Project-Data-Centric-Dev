881e6a443d30645a9c6b13ce2cc09941fcda2006
grisselfaura <grissel.faura@gmail.com>
2020-10-07 01:54:49 +0000

    Conneced add_recipe new input to app.py

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// Initialize Materialize Elements
 $(document).ready(function(){
     $(".sidenav").sidenav();
     $(".collapsible").collapsible();
@@ -7,8 +8,12 @@ $(document).ready(function(){
     $(".modal").modal();
     $("input#input_text, textarea#textarea2, textarea#recipe_description, textarea#basic_ingredients, textarea#complementary_ingredients, textarea#category_method"
 ).characterCounter();
+    $('.cancel-form').click(function() {
+        window.history.back();
+    });
     
-    // Code provided from the explanatory videos from the Course 
+
+// Code provided from the explanatory videos from the Course 
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
@@ -37,4 +42,60 @@ $(document).ready(function(){
         });
     }
 });
-    
+
+// Code template approach share from previous students
+// Add and Remove Ingredients
+ var ingredientField = $(".ingredient").length;
+    $("#add_ingredient").on("click", function () {
+        $("select").formSelect("destroy");
+        $(".new-ingredient:first").clone().insertBefore("#add_ingredient").find("input[type='text'], select, textarea").val("");
+        $("select").formSelect();
+        ingredientField += 1;
+    });
+
+    $("#remove_ingredient").on("click", function () {
+        if (ingredientField > 1) {
+            /* only remove the :last item */
+            $(this).siblings(".new-ingredient:last").remove();
+            /* ensure original ingredient line never gets deleted */
+            ingredientField-= 1;
+        }
+    });
+
+      
+
+// Add and Remove Extra Ingredients
+ var extraIngredientField = $(".compIngredient").length;
+    $("#add_comp_ingredient").on("click", function () {
+        $("select").formSelect("destroy");
+        $(".new-CompIngredient:first").clone().insertBefore("#add_comp_ingredient").find("input[type='text'], select, textarea").val("");
+        $("select").formSelect();
+        extraIngredientField += 1;
+    });
+
+    $("#remove_comp_ingredient").on("click", function () {
+        if (extraIngredientField > 1) {
+            /* only remove the :last item */
+            $(this).siblings(".new-CompIngredient:last").remove();
+            /* ensure original ingredient line never gets deleted */
+            extraIngredientField-= 1;
+        }
+    });
+
+// Add and Remove preparation steps
+var preparationField = $(".preparation").length;
+    /* add new cloned item */
+    $("#add_step").on("click", function () {
+        $(".new-step:first").clone().insertBefore("#add_step").find("input[type='text'], select, textarea").val("");
+        /* increase counter so original direction is never removed */
+       preparationField += 1;
+    });
+    /* delete last cloned item */
+    $("#remove_step").on("click", function () {
+        if ( preparationField > 1) {
+            /* only remove the :last item */
+            $(this).siblings(".new-step:last").remove();
+            /* ensure original direction line never gets deleted */
+            preparationField-= 1;
+        }
+    });
